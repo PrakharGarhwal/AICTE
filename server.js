c1f5ab8f6b0d395bfcc92d5987cba32ee4e58b3c
PrakharGarhwal <prakhargarhwal102@gmail.com>
2026-04-27 00:41:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.delete('/api/curriculum/:id', async (req, res) => {
 
 const PORT = process.env.PORT || 3000; 
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
